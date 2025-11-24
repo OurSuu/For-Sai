c7dbdbc109b8ec56ee27f9367b9d82cf447d3640
OurSuu <Noppanatyukun@gmail.com>
2025-11-24 16:51:23 +0700

    Fix vite config for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "For-Sai", // <--- ใส่ชื่อ Repo ที่ตั้งใน GitHub ตรงนี้ (อย่าลืม / ปิดหัวท้าย)
 })
